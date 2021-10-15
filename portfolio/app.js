abbe0813db1f3b57869c2692c50cefe1b1195069
Siddhi-244 <siddhibhanushali1234@gmail.com>
2021-10-15 16:49:52 +0530

    qualifications section

--- portfolio/app.js
+++ portfolio/app.js
@@ -37,3 +37,20 @@ function toggleSkills(){
 skillsHeader.forEach((el)=>{
 	el.addEventListener("click",toggleSkills)
 	});
+
+
+const tabs=document.querySelectorAll("[data-target]");
+const tabContents=document.querySelectorAll("[data-content]");
+tabs.forEach(tab=>{
+	tab.addEventListener("click",()=>{
+		const target=document.querySelector(tab.dataset.target)
+		tabContents.forEach(tabContent => {
+			tabContent.classList.remove("qualification__active")
+		})
+		target.classList.add("qualification__active")
+		tabs.forEach(tab=>{
+			tab.classList.remove("qualification__active")
+		})
+		tab.classList.add("qualification__active");
+	})
+})
