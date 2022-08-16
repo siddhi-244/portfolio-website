7f90d54711895032cd717cbe5e27c0bd3e726d99
Siddhi-244 <siddhibhanushali1234@gmail.com>
2022-08-16 13:49:06 +0530

    added footer

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,70 +1,96 @@
-const navMenu=document.getElementById("nav-menu");
-const navToggle=document.getElementById("nav-toggle");
-const navClose=document.getElementById("nav-close");
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
 
-if(navToggle){
-	navToggle.addEventListener("click",function(){
-		navMenu.classList.add("show-menu");
-	});
+if (navToggle) {
+  navToggle.addEventListener("click", function () {
+    navMenu.classList.add("show-menu");
+  });
 }
-if(navClose){
-	navClose.addEventListener("click",function(){
-		navMenu.classList.remove("show-menu");
-	});
+if (navClose) {
+  navClose.addEventListener("click", function () {
+    navMenu.classList.remove("show-menu");
+  });
 }
-const navLink=document.querySelectorAll(".nav__link");
-function linkAction(){
-	const navMenu=document.getElementById("nav-menu");
-	navMenu.classList.remove("show-menu");
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener("click",linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-const skillsContent=document.getElementsByClassName("skills__content");
-const skillsHeader=document.querySelectorAll(".skills__header");
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
 
-
-function toggleSkills(){
-	console.log(skillsContent);
-	let itemClass=this.parentNode.className;
-	for(i=0;i<skillsContent.length;i++){
-		skillsContent[i].className="skills__content skills__close";
-	}
-	if(itemClass=="skills__content skills__close"){
-		this.parentNode.className="skills__content skills__open";
-	
+function toggleSkills() {
+  console.log(skillsContent);
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass == "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
 }
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("qualification__active");
+    });
+    target.classList.add("qualification__active");
+    tabs.forEach((tab) => {
+      tab.classList.remove("qualification__active");
+    });
+    tab.classList.add("qualification__active");
+  });
+});
+
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
 }
-skillsHeader.forEach((el)=>{
-	el.addEventListener("click",toggleSkills)
-	});
+window.addEventListener("scroll", scrollActive);
 
-
-const tabs=document.querySelectorAll("[data-target]");
-const tabContents=document.querySelectorAll("[data-content]");
-tabs.forEach(tab=>{
-	tab.addEventListener("click",()=>{
-		const target=document.querySelector(tab.dataset.target)
-		tabContents.forEach(tabContent => {
-			tabContent.classList.remove("qualification__active")
-		})
-		target.classList.add("qualification__active")
-		tabs.forEach(tab=>{
-			tab.classList.remove("qualification__active")
-		})
-		tab.classList.add("qualification__active");
-	})
-})
-
-
-let swiper = new Swiper('.portfolio__container', {
-        cssMode: true,
-        loop:true,
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable:true,
-        },
-      });
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
